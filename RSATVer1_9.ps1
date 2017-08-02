91e68614687281390e329283b5bf3d0fa4b99361
LordHagen <olehag04@nfk.no>
2017-08-02 15:33:05 +0200

    Multiple minor text edits
    Translated to english, and minor text edits.

--- RSATVer1_9.ps1
+++ RSATVer1_9.ps1
@@ -1,10 +1,12 @@
 ﻿#Lord Hagen / olehag04@nfk.no
+#Rev1.9
 
+#next line might be needed.
 #if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 
 Write-Host ""
-Write-Host "Oppgi Passord"
+Write-Host "Input Credentials"
 Write-Host ""
 
 $creds = Get-Credential $env:USERDOMAIN\$env:USERNAME-admin
@@ -12,12 +14,12 @@ Clear-Host
 
 do {
         do { 
-            Write-Host "`n`tRemote Server Administration Tools v1.9`n" -ForegroundColor Magenta
-            Write-Host "`t`tHovedmeny`n" -ForegroundColor Cyan
+            Write-Host "`n`tRSAT Launcher`n" -ForegroundColor Magenta
+            Write-Host "`t`tMain Meny`n" -ForegroundColor Cyan
             Write-Host "`t`t`t1. Users & Computers" -ForegroundColor Green
             Write-Host "`t`t`t2. Group Policy Object" -ForegroundColor Green
             Write-Host "`t`t`t3. Print Management" -ForegroundColor Green
-            Write-Host "`t`t`t4. Feil Passord" -ForegroundColor Green
+            Write-Host "`t`t`t4. Wrong password" -ForegroundColor Green
             Write-Host ""
             Write-Host "`t`tX - Avslutt`n" -ForegroundColor Red
             Write-Host ""
@@ -26,13 +28,13 @@ do {
             $choice = read-host
             
             Write-Host ""
-            #   Legg til nr. (E.g. [1-99x]) for testing.
+            #       Add nr. (E.g. [1-99x]) for testing purposes.
             $ok = $choice -match '^[1-4x]+$'
             
             if ( -not $ok) 
                 {
                     Clear-Host
-                    Write-Host "Ugyldig valg" -ForegroundColor Red
+                    Write-Host "Invalid choice" -ForegroundColor Red
                 }
 
             } until ( $ok )
@@ -45,31 +47,31 @@ do {
                 "1" #Users & Computers.
                 {
                     Start-Process powershell -workingdirectory $PSHOME -Credential $creds -ArgumentList "/c dsa.msc"
-                    Start-Sleep -s 2
+                    Start-Sleep -s 1
                     Clear-Host
                 }
             
                 "2" #Group Policy Object
                 {
                     Start-Process powershell -workingdirectory $PSHOME -Credential $creds -ArgumentList "/c gpmc.msc"
-                    Start-Sleep -s 2
+                    Start-Sleep -s 1
                     Clear-Host
                 }
 
                 "3" #Print Management
                 {
                     Start-Process powershell -workingdirectory $PSHOME -Credential $creds -ArgumentList "/c printmanagement.msc"
-                    Start-Sleep -s 2
+                    Start-Sleep -s 1
                     Clear-Host
                 }
 
-                "4" #Feil Passord
+                "4" #Wrong password
                 {
                     $creds = Get-Credential $env:USERDOMAIN\$env:USERNAME-admin
                     Clear-Host
                 }
             
-                "5" #Test her.
+                "5" #Testing.
                 {
                     #Start-Process powershell -workingdirectory $PSHOME -Credential $creds -ArgumentList "/c xxx.msc"
                 }
